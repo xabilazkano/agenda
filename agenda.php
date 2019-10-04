cb06176a88c9fbd93ff8d5d602fa5b16b1033c74
xabilazkano <xlazcanoit18dw@ikzubirimanteo.com>
2019-10-04 09:53:46 +0200

    empezando con sesiones

---
+++ agenda.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+?>
+<html>
+<head>
+	<title>AGENDA</title>
+</head>
+<body>
+
+	<center>
+		<h1>Introduce tus datos</h1>
+		<form method="Post" action="agenda.php">
+			Nombre: <input type="text" name="izena"><br><br>
+			Email: <input type="text" name="email"><br><br>
+			<input type="submit" name="bidali" value="Enviar">
+		</form>
+		<aside>
+			<h2>Agenda</h2>
+			<p  id="informacion" style="border: 1px solid black;height: 200px;width: 300px;">
+				
+				<?php
+				if(!isset($_SESSION["agenda"])){
+					$_SESSION["agenda"]= array();
+				}
+				else{
+					$agenda=$_SESSION["agenda"];
+
+					if($_POST["bidali"]){
+						$agenda[$_POST["izena"]]=$_POST["email"];
+						foreach ($agenda as $key => $value) {
+							echo count($agenda);
+						}
+					}
+
+				}
+				
+				?>
+			</p>
+		</aside>
+	</center>
+</body>
+</html>
