3bbb77164f898c59a4ce420c8ba8fdf136996c46
xabilazkano <xlazcanoit18dw@ikzubirimanteo.com>
2019-10-07 12:54:57 +0200

    Comprueba que se introduce el nombre y el correo

--- agenda.php
+++ agenda.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+if(!isset($_SESSION["agenda"])){
+	$_SESSION["agenda"]= array();
+}
 ?>
 <html>
 <head>
@@ -9,8 +12,17 @@ session_start();
 	<center>
 		<h1>Introduce tus datos</h1>
 		<form method="Post" action="agenda.php">
-			Nombre: <input type="text" name="izena"><br><br>
-			Email: <input type="text" name="email"><br><br>
+			Nombre: <input type="text" name="izena"<?php
+			if(isset($_POST["izena"])){
+				echo "value=".$_POST['izena'];
+			}
+			?>><br><br>
+			Email: <input type="text" name="email"<?php
+			if(isset($_POST["email"])){
+				echo "value=".$_POST['email'];
+			}
+			?>
+			><br><br>
 			<input type="submit" name="bidali" value="Enviar">
 		</form>
 		<aside>
@@ -18,21 +30,37 @@ session_start();
 			<p  id="informacion" style="border: 1px solid black;height: 200px;width: 300px;">
 				
 				<?php
-				if(!isset($_SESSION["agenda"])){
-					$_SESSION["agenda"]= array();
-				}
-				else{
-					$agenda=$_SESSION["agenda"];
+				
+				
+					
 					if($_POST["bidali"]){
-						$agenda[$_POST["izena"]]=$_POST["email"];
-						foreach ($agenda as $key => $value) {
-							echo "<br>Nombre: ".$key."<br>";
-							echo "Correo: ".$value."<br>";
+						$agenda=$_SESSION["agenda"];
+						if(trim($_POST["izena"])!=""){
+							if(!array_key_exists($_POST["izena"],$agenda)){
+								$agenda[$_POST["izena"]]=$_POST["email"];
+								foreach ($agenda as $key => $value) {
+									echo "<br>Nombre: ".$key."<br>";
+									echo "Correo: ".$value."<br>";
+								}
+							}
+							else{
+								if(trim($_POST["email"])!=""){
+									$agenda[$_POST["izena"]]=$_POST["email"];
+									foreach ($agenda as $key => $value) {
+										echo "<br>Nombre: ".$key."<br>";
+										echo "Correo: ".$value."<br>";
+									}
+								}
+								else{
+									echo "No has introducido ningun correo";
+								}
+							}
 						}
+						else{
+							echo "No has introducido el nombre";
+						}	
 					}
 					$_SESSION["agenda"]=$agenda;
-				}
-				
 				?>
 			</p>
 		</aside>
