42858d71d305ad958ca76748049e255c35147a6b
xabilazkano <xlazcanoit18dw@ikzubirimanteo.com>
2019-10-07 13:54:44 +0200

    Index añadido

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>AGENDA</title>
+</head>
+<body>
+	<center>
+		<h1>Introduce tu nombre</h1>
+		<form method="Post" action="agenda.php">
+			Nombre: <input type="text" name="nombre"><br><br>
+			<input type="submit" name="enviar" value="Enviar">
+		</form>
+	</center>
+</body>
+</html>
